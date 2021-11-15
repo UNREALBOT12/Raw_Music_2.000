da289945bd35a9d9b6034ea1318028cdb7fa020b
UNREALBOT12 <91039807+UNREALBOT12@users.noreply.github.com>
2021-11-15 21:19:53 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,8 +1,8 @@
 import requests
 from pyrogram import Client as Bot
 
-from XmartyMusic.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
-from XmartyMusic.services.callsmusic import run
+from config import API_HASH, API_ID, BOT_TOKEN
+from services.callsmusic import run
 
 response = requests.get(BG_IMAGE)
 file = open("./etc/foreground.png", "wb")
